f323efb7ff8af8142193117a4079ad953ceb033c
Maritreye <pandeymaritreye@gmail.com>
2025-02-05 10:50:35 +0530

    Add files via upload

---
+++ project 1 c.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+	int number,guess,nguesses=1;
+	srand(time(0));//helps to give different numbers after 0 second
+	number = rand()%100 + 1;// generates number between 1 to 100
+	//printf("The number is %d\n",number); //if you include this line than the number will be shown
+	//running loop until number is guessed
+	do{
+	printf("guess the number\n");
+	scanf("%d", &guess);
+	if(guess<number){
+		printf("higher number please\n");
+		}
+		else if(guess>number){
+			printf("lower number plese\n");
+		}	
+		else{
+			printf("You guessed it in%d attempts\n",nguesses);
+		}
+		nguesses++;
+	}
+	while(guess!=number);
+	return 0;
+}
